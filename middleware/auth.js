c3882f5c809a69cf19f40eb9736d9c65d8b8bb48
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-08-17 11:12:07 +0100

    comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,15 @@
+// import dependencies
 const jwt = require('jsonwebtoken')
 const apiResponses = require('../utils/apiResponses')
-
 const SECRET_KEY = process.env.JWT_SECRET_KEY
 
+// decode authorization header and provide attached decoded data to request
 const decode = (req, res, next) => {
-
   if (!req.headers['authorization']) return res.status(401).send(apiResponses.error("User not logged in", res.statusCode))
 
   try {
     const token = req.headers.authorization
     const decoded = jwt.decode(token, SECRET_KEY)
-    // console.log(decoded)
     req.information = decoded
     return next()
 
@@ -20,3 +19,4 @@ const decode = (req, res, next) => {
 }
 
 module.exports = { decode }
+
