cfe329d9f85e431c153eb0f6a20a62c7f7d6de2e
Stephen Hanna <stephenh126_@hotmail.co.uk>
2024-02-21 22:12:15 +0000

    minio connection model

---
+++ models/minio.js
@@ -0,0 +1,16 @@
+var Minio = require('minio')
+
+const host = process.env.MINIO_HOST
+const port = process.env.MINIO_PORT
+const accessKey = process.env.MINIO_ACCESS_KEY
+const secretKey = process.env.MINIO_SECRET_KEY
+
+var minioClient = new Minio.Client({
+  endPoint: host,
+  port: port,
+  useSSL: false,
+  accessKey: accessKey,
+  secretKey: secretKey,
+})
+
+module.exports = minioClient;
