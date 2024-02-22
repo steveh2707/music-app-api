3d01d1d51c8b9a1dcc22127899927cbb952d59a5
Stephen Hanna <stephenh126_@hotmail.co.uk>
2024-02-22 12:04:04 +0000

    ssl env variable added

--- models/minio.js
+++ models/minio.js
@@ -4,11 +4,12 @@ const host = process.env.MINIO_HOST
 const port = process.env.MINIO_PORT
 const accessKey = process.env.MINIO_ACCESS_KEY
 const secretKey = process.env.MINIO_SECRET_KEY
+const ssl = process.env.MINIO_USE_SSL
 
 var minioClient = new Minio.Client({
   endPoint: host,
   port: 1 * port,
-  useSSL: false,
+  useSSL: (ssl === 'true'),
   accessKey: accessKey,
   secretKey: secretKey,
 })
